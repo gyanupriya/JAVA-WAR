7ca52727b42b7eff7633b4e7f445c67aaad48f6c
gyanupriya <gyanu.er@gmail.com>
2018-09-03 12:30:54 +0530

    Added test file

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -23,4 +23,9 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.war
+
+rm /var/lib/tomcat7/webapps/*.war
+cp /var/lib/jenkins/workspace/Pipeline-java-war/target/sparkjava-hello-world-*.war /var/lib/tomcat7/webapps/
+
+
